50bdeeb21fada7b255108c45f538f8ad96722a85
Donald Clark Jackson <dcj@clark-communications.com>
2013-06-05 13:25:19 -0700

    Checkpoint.  I do not remember making this changes in the first place...

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject clj-oauth2 "0.1.1"
+(defproject net.likestream/clj-oauth2 "0.1.1"
   :description "clj-http and ring middlewares for OAuth 2.0"
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [org.clojure/data.json "0.1.1"]
-                 [clj-http "0.2.6"]]
+                 [org.clojure/data.json "0.1.2"] ;; bumped from 0.1.1
+                 [clj-http "0.3.2"] ;; bumped form 0.2.6
+                 ]
   :dev-dependencies [[ring "0.3.11"]
                      [com.stuartsierra/lazytest "1.1.2"]]
   :repositories {"stuartsierra-releases" "http://stuartsierra.com/maven2"}
